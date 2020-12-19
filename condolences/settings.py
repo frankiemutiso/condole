6bada3a08d5a874627e6808ac9ec4a47d41d3cf7
Francis Mutiso <franckmutiso@gmail.com>
2020-12-19 14:54:02 +0300

    CONFIGURED POSTGRES DB

--- condolences/settings.py
+++ condolences/settings.py
@@ -80,11 +80,32 @@ WSGI_APPLICATION = 'condolences.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# POSTGRESQL
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': env('DATABASE_NAME'),
+
+        'USER': env('DATABASE_USER'),
+
+        'PASSWORD': env('DATABASE_PASSWORD'),
+
+        'HOST': env('DATABASE_HOST'),
+
+        'PORT': env('DATABASE_PORT'),
+
     }
+
 }
 
 
