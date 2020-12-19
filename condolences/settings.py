6f07f93ae50c15b4236f4629e4fb9af61379c4c4
Francis Mutiso <franckmutiso@gmail.com>
2020-12-19 22:04:44 +0300

    MINOR CHANGES ON SETTINGS FILE

--- condolences/settings.py
+++ condolences/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
-from condolences.condole.views import condolences
 from pathlib import Path
 
 import environ
@@ -22,7 +20,6 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
