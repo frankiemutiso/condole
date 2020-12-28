d437c123855796f749c8c4d076a1a3cb3da7eaac
Francis Mutiso <franckmutiso@gmail.com>
2020-12-28 21:48:15 +0300

    minor routing changes

--- condole/urls.py
+++ condole/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('deaths/<int:pk/leave-message', views.leave_message, name='leave-message'),
-    path('condolences/', views.condolences, name='condolences'),
-    path('deaths/', views.deaths, name='deaths'),
+    path('', views.deaths, name='deaths'),
     path('deaths/<int:pk>/', views.detail, name='detail'),
+    path('deaths/<int:pk/leave-message/', views.leave_message, name='leave-message'),
+    path('condolences/', views.condolences, name='condolences'),
     path('create-death/', views.create_death, name='create-death')
 ]
