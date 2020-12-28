f9548a1569955bd51ac586cd379259684039fa22
Francis Mutiso <franckmutiso@gmail.com>
2020-12-28 22:18:46 +0300

    minor changes on urls.py

--- condole/urls.py
+++ condole/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.deaths, name='deaths'),
     path('deaths/<int:pk>/', views.detail, name='detail'),
-    path('deaths/<int:pk/leave-message/', views.leave_message, name='leave-message'),
+    path('deaths/<int:pk>/leave-message/',
+         views.leave_message, name='leave-message'),
     path('condolences/', views.condolences, name='condolences'),
     path('create-death/', views.create_death, name='create-death')
 ]
