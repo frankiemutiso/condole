f95026353259d12b8369524215e00e4912c39910
Francis Mutiso <franckmutiso@gmail.com>
2020-12-31 19:36:55 +0300

    removed save method from Death model

--- condole/models.py
+++ condole/models.py
@@ -7,14 +7,10 @@ class Death(models.Model):
     birth_year = models.IntegerField(null=True)
     death_year = models.IntegerField(null=True)
     description = models.TextField()
-    slug = models.SlugField(unique=True, null=True, blank=True)
+    slug = models.SlugField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Death, self).save(*args, **kwargs)
 
 
 class Message(models.Model):
