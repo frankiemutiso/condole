d0fef94e0ddd237e25b9ea74abfbde9f97389d14
Francis Mutiso <franckmutiso@gmail.com>
2020-12-31 17:54:12 +0300

    reconfigured views

--- condole/views.py
+++ condole/views.py
@@ -31,8 +31,8 @@ def deaths(request):
     return render(request, 'condole/deaths.html', context)
 
 
-def detail(request, pk):
-    death = Death.objects.get(pk=pk)
+def detail(request, slug):
+    death = Death.objects.get(slug=slug)
     messages = Message.objects.filter(owner=death.id)
 
     context = {'death': death, 'messages': messages}
@@ -40,14 +40,14 @@ def detail(request, pk):
     return render(request, 'condole/detail.html', context)
 
 
-def leave_message(request, pk):
+def leave_message(request, slug):
     form = MessageForm()
     if request.method == 'POST':
         form = MessageForm(request.POST)
         if form.is_valid():
-            death = Death.objects.get(pk=pk)
+            death = Death.objects.get(slug=slug)
             message = Message()
-            message.owner = Death.objects.get(pk=pk)
+            message.owner = Death.objects.get(slug=slug)
             message.name = form.cleaned_data['name']
             message.relationship_with_deceased = form.cleaned_data['relationship_with_deceased']
             message.email = form.cleaned_data['email']
@@ -58,4 +58,3 @@ def leave_message(request, pk):
             return redirect('detail', death.slug)
 
     return render(request, 'condole/leave-message.html', {'form': form})
-
